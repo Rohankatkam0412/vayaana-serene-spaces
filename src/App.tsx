b04749f1d4ae5315583ea57895be8a60988e30f2
ritikkatkam123 <ritik.katkam@colostate.edu>
2026-05-02 21:48:02 -0500

    error fixes

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
